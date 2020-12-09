607cc3271cbea9381f89e28333c19febaa9ca096
vivisantosa <65078785+vivisantosa@users.noreply.github.com>
2020-12-08 20:46:32 -0500

    Files for Tableau
    files for Tableau

---
+++ Codes/ML-vivi/multivariate_regression/tpot_mnst1.txt/pipeline_gen_3_idx_0_2020.11.14_12-57-55.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=23)
+
+# Average CV score on the training set was: 0.6433111448954526
+exported_pipeline = MultinomialNB(alpha=1.0, fit_prior=False)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 23)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
---
+++ Codes/ML-vivi/multivariate_regression/tpot_mnst1.txt/pipeline_gen_9_idx_1_2020.11.14_14-08-22.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import RFE
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.pipeline import make_pipeline, make_union
+from tpot.builtins import StackingEstimator
+from tpot.export_utils import set_param_recursive
+from sklearn.preprocessing import FunctionTransformer
+from copy import copy
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=23)
+
+# Average CV score on the training set was: 0.6553707123869327
+exported_pipeline = make_pipeline(
+    make_union(
+        RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.5, n_estimators=100), step=0.55),
+        FunctionTransformer(copy)
+    ),
+    BernoulliNB(alpha=0.1, fit_prior=False)
+)
+# Fix random state for all the steps in exported pipeline
+set_param_recursive(exported_pipeline.steps, 'random_state', 23)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
